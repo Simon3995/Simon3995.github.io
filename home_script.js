775694713512dc8d80d9e422b91d03d6e7a637c2
shnimon <33228504+Simon3995@users.noreply.github.com>
2025-06-03 14:13:57 +0200

    Added two new title animations

--- home_script.js
+++ home_script.js
@@ -1,7 +1,81 @@
-let title, chars;
+let title, chars, loops = [];
 
 document.addEventListener("DOMContentLoaded", function () {
     title = document.getElementById("title");
+    
+    // horizontal wave animation
+    loops.push(function loop_0() {
+        requestAnimationFrame(loop_0);
+        for (let i = 0; i < chars.length; i++) {
+            let x = Number(new Date()) / 5e2;
+            chars[i].style.transform = `translateX(${8 * Math.sin(x + i)}px)`;
+        }
+    });
+
+    // vertical wave animation
+    loops.push(function loop_1() {
+        requestAnimationFrame(loop_1);
+        for (let i = 0; i < chars.length; i++) {
+            let x = Number(new Date()) / 5e2;
+            chars[i].style.transform = `translateY(${8 * Math.sin(x + i)}px)`;
+        }
+    });
+
+    // letters swinging back and forth
+    loops.push(function loop_2() {
+        requestAnimationFrame(loop_2);
+        for (let i = 0; i < chars.length; i++) {
+            let x = Number(new Date()) / 5e2;
+            chars[i].style.transform = `rotate(${8 * Math.sin(x + i)}deg)`;
+        }
+    });
+
+    // letters flipping backwards
+    loops.push(function loop_3() {
+        requestAnimationFrame(loop_3);
+        for (let i = 0; i < chars.length; i++) {
+            let x = Number(new Date()) / 5e2;
+            chars[i].style.transform = `rotate3d(0, 1, 0, ${theta(x - i/3)}rad)`;
+        }
+    });
+
+    // somersaulting letters
+    loops.push(function loop_4() {
+        requestAnimationFrame(loop_4);
+        for (let i = 0; i < chars.length; i++) {
+            let x = Number(new Date()) / 8e2;
+            chars[i].style.transform = `rotate3d(0, 0, 1, ${2*theta(x - i/3)}rad)`;
+        }
+    });
+
+    // letters dipping down
+    loops.push(function loop_5() {
+        requestAnimationFrame(loop_5);
+        for (let i = 0; i < chars.length; i++) {
+            let x = Number(new Date()) / 8e2;
+            chars[i].style.transform = `translateY(${20*Math.sin(theta(x - i/3))}px)`;
+        }
+    });
+
+    // random translation at random intervals
+    loops.push(function loop_6() {
+        requestAnimationFrame(loop_6);
+        for (let i = 0; i < chars.length; i++) {
+            if (Math.random() < 0.01) {
+                chars[i].style.transform = `translate(${10 * (Math.random()-.5)}px, ${10 * (Math.random()-.5)}px)`;
+            }
+        }
+    });
+
+    // random scaling at random intervals
+    loops.push(function loop_7() {
+        requestAnimationFrame(loop_7);
+        for (let i = 0; i < chars.length; i++) {
+            if (Math.random() < 0.01) {
+                chars[i].style.transform = `scale(${1 + 0.7 * (Math.random()-.5)}, ${1 + 0.7 * (Math.random()-.5)})`;
+            }
+        }
+    });
     
     // wrap each letter in a span element
     let text = title.textContent;
@@ -13,64 +87,9 @@ document.addEventListener("DOMContentLoaded", function () {
     chars = Array.from(document.getElementsByClassName("char"));
 
     // choose random action
-    let index = Math.floor(Math.random() * 6)
-    //let index = 6;
-    window[`loop_${index}`]();
+    let index = Math.floor(Math.random() * loops.length)
+    loops[index]();
 });
-
-function loop_0() {
-    requestAnimationFrame(loop_0);
-
-    for (let i = 0; i < chars.length; i++) {
-        let x = Number(new Date()) / 5e2;
-        chars[i].style.transform = `translateX(${8 * Math.sin(x + i)}px)`;
-    }
-}
-
-function loop_1() {
-    requestAnimationFrame(loop_1);
-
-    for (let i = 0; i < chars.length; i++) {
-        let x = Number(new Date()) / 5e2;
-        chars[i].style.transform = `translateY(${8 * Math.sin(x + i)}px)`;
-    }
-}
-
-function loop_2() {
-    requestAnimationFrame(loop_2);
-
-    for (let i = 0; i < chars.length; i++) {
-        let x = Number(new Date()) / 5e2;
-        chars[i].style.transform = `rotate(${8 * Math.sin(x + i)}deg)`;
-    }
-}
-
-function loop_3() {
-    requestAnimationFrame(loop_3);
-
-    for (let i = 0; i < chars.length; i++) {
-        let x = Number(new Date()) / 5e2;
-        chars[i].style.transform = `rotate3d(0, 1, 0, ${theta(x - i/3)}rad)`;
-    }
-}
-
-function loop_4() {
-    requestAnimationFrame(loop_4);
-
-    for (let i = 0; i < chars.length; i++) {
-        let x = Number(new Date()) / 8e2;
-        chars[i].style.transform = `rotate3d(0, 0, 1, ${2*theta(x - i/3)}rad)`;
-    }
-}
-
-function loop_5() {
-    requestAnimationFrame(loop_5);
-
-    for (let i = 0; i < chars.length; i++) {
-        let x = Number(new Date()) / 8e2;
-        chars[i].style.transform = `translateY(${20*Math.sin(theta(x - i/3))}px)`;
-    }
-}
 
 function theta(t, T = 15, rampRatio = 0.1) {
     let rampTime = T * rampRatio / 2; // ramp up and ramp down
