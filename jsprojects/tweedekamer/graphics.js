9a68d26d433f59520d82e8357b9775ed41ed02c3
shnimon <33228504+Simon3995@users.noreply.github.com>
2025-06-03 14:21:35 +0200

    Added latest Peilingwijzer to TweedeKamer

--- jsprojects/tweedekamer/graphics.js
+++ jsprojects/tweedekamer/graphics.js
@@ -92,7 +92,7 @@ const drawYearMenu = function() {
       	ctx.fillStyle = "#0390fc";
 		ctx.font = "bold 16px Arial";
 		ctx.textAlign = "center";
-		ctx.fillText((lang=="nl")?"EenVandaag Peiling 29 april 2025":"EenVandaag Poll 29 April 2025", c.width/2, 440);
+		ctx.fillText((lang=="nl")?"Peilingwijzer 27 mei 2025":"Polling Average 27th May 2025", c.width/2, 440);
 		
 		// draw year text
 		ctx.fillStyle = "#0390fc";
