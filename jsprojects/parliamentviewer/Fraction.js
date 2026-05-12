0522838357d2e53b4e5853cd41db7dfddafed3fa
Simon van Raaij <33228504+Simon3995@users.noreply.github.com>
2026-05-12 17:38:47 +0200

    Changed parliamentviewer link to external URL

--- jsprojects/parliamentviewer/Fraction.js
+++
@@ -1,8 +0,0 @@
-// a class for a party fraction in one election
-class Fraction {
-    constructor(party, seat_amt) {
-        this.party = party;
-        this.seat_amt = seat_amt;
-        this.seat_centers = [];
-    }
-}
